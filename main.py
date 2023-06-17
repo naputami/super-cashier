df3adc636bde437661d505c0f96fa278f2465790
Nadia Puji Utami <nadiapujiutami@gmail.com>
2023-06-17 13:11:57 +0700

    add function for showing items in cart

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ while True:
     4. Update item price in cart
     5. Update item quantity in cart
     6. Delete an item in cart
-    7. Delete all items.
+    7. Reset Transaction.
     8. Check out order
     9. Quit program
     ''')
 
     user_input = input("chosen menu: ")
     if user_input == "1":
-        item_name = input("item name: ")
+        item_name = input("item name: ").title()
         item_price = input("item price: ")
         item_qty = input("item quantity: ")
         trnsct_123.add_item(item_name, item_qty, item_price)
@@ -32,10 +32,10 @@ while True:
         trnsct_123.check_order()
 
     elif user_input in ["3", "4", "5"]:
-        item_name_update = input("item name to be updated: ")
+        item_name_update = input("item name to be updated: ").title()
         if trnsct_123.search_name(item_name_update): 
             if user_input == "3":
-                new_name = input("new name: ")
+                new_name = input("new name: ").title()
                 trnsct_123.update_name(item_name_update, new_name)
             elif user_input == "4":
                 new_price = input("new price: ")
@@ -47,7 +47,7 @@ while True:
             print(f'{item_name_update} is not found in cart.')
 
     elif user_input == "6":
-        item_name_delete = input("item name to be deleted: ")
+        item_name_delete = input("item name to be deleted: ").title()
         if trnsct_123.search_name(item_name_delete):
             trnsct_123.delete_item(item_name_delete)
         else:
@@ -60,9 +60,10 @@ while True:
         checkout_data = trnsct_123.check_out()
         #insert check outed item data to database
         insert_to_database("checkout.db", checkout_data)
+        break
         
     elif user_input == "9":
-        print("Thank you for shopping here.")
+        print("Thank you for using this program")
         print("Closing the program ...")
         break
     
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Super Cashier
+
+## Background
+Super cashier is self-service app for 
